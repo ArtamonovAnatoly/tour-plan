91251dd01881403fa96f609adab251e509764d56
ArtamonovAnatoly <79615328922@yandex.ru>
2021-11-03 19:03:19 +0300

    create: added form validation

--- js/main.js
+++ js/main.js
@@ -44,9 +44,7 @@ modalOverlay.addClass('modal__overlay--visible');
 modalDialog.addClass('modal__dialog--visible');
 
 // When the modal is shown, we want a fixed body
-document.body.style.position = 'fixed';
-document.body.style.top = `-${window.scrollY}px`;
-
+document.body.style.overflow = 'hidden';
 }
 function closeModal(event) {
   event.preventDefault();
@@ -54,14 +52,30 @@ var modalOverlay = $('.modal__overlay');
 var modalDialog = $('.modal__dialog');
 modalOverlay.removeClass('modal__overlay--visible');
 modalDialog.removeClass('modal__dialog--visible');
-// Когда модальное окно скрыто...
-//const scrollY = document.body.style.top;
-document.body.style.position = '';
-document.body.style.top = '';
-window.scrollTo(0, parseInt(scrollY || '0') * -1);
+// When the modal is hidden, we want to remain at the top of the scroll position
+document.body.style.overflow = 'visible';
 
 }
 
+//обработка форм
+$('.form').each(function(){
+$(this).validate({
+  errorClass: "invalid",
+  messages: {
+    name: {
+      required: "Please specify your name",
+      minlength: "name, required to contain minimum 2 symbols"
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    },
+    phone: {
+      required: "Phone required",
+    },
+  },
+});
+});
 
 
 
